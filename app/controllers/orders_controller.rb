567ce187975e389b2494c85c6f29b169ae8557b5
priyanka1-webonise <ukirde.priyanka@webiniselab.com>
2013-01-31 12:56:26 +0530

    working on product id duplication for line items

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,4 +1,5 @@
 class OrdersController < ApplicationController
+
   def update
     logger.info "########################################### update #{params.inspect}"
     @new_item_quantity = (params[:order][:line_items][:product_quantity])
@@ -8,20 +9,21 @@ class OrdersController < ApplicationController
     @order=Order.find(params[:id]) rescue nil
     @line_items_all=@order.line_items
     logger.info "########################################### items #{ @line_items_all.inspect}"
-    @line_items_all.each do |line_item|
-      if line_item.product_id ==  @new_item_product_id
-         line_item.product_quantity =  @new_item_quantity +  line_item.product_quantity
-         logger.info "########################################### add #{ line_item.product_quantity.inspect}"
-      else
-        @order_line_items = @order.line_items.build(params[:order][:line_items])
-        @order_line_items.sub_total = @order_line_items.calculate_subtotal
-      end
-    end
-    ##@order_line_items = @order.line_items.build(params[:order][:line_items])
-    #@order_line_items.sub_total = @order_line_items.calculate_subtotal
-    #@order_line_items.save
+    #@line_items_all.each do |line_item|
+     # if line_item.product_id ==  @new_item_product_id
+      #   line_item.product_quantity =  @new_item_quantity +  line_item.product_quantity
+       #  logger.info "########################################### add #{ line_item.product_quantity.inspect}"
+      #else
+      #  @order_line_items = @order.line_items.build(params[:order][:line_items])
+      #  @order_line_items.sub_total = @order_line_items.calculate_subtotal
+      #end
+    #end
+    @order_line_items = @order.line_items.build(params[:order][:line_items])
+    @order_line_items.sub_total = @order_line_items.calculate_subtotal
+    @order_line_items.save
     respond_to do |format|
       format.js
     end
   end
+
 end
--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,24 +1,15 @@
 class ProductsController < ApplicationController
-
   def index
-    #respond_to do |format|
-     # format.js
-    #end
     if current_user.present?
       @products=Product.all
-      #@order = Order.new
       @order = Order.where(:set_order => false).first
       unless @order.present?
         @order = Order.create(:user_id=>current_user.id)
       end
-      logger.info "########################################### order #{@order.inspect}"
-
     end
   end
 
   def show
     @product=Product.find(params[:product_id])
-
-
   end
 end
