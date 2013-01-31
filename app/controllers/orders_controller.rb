8a1f52a981f708a789c69cffb75bc41e5a6bf610
priyanka1-webonise <ukirde.priyanka@webiniselab.com>
2013-01-31 11:56:00 +0530

    working on line_item update function

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,40 +1,27 @@
 class OrdersController < ApplicationController
-
-
-
   def update
-
-
     logger.info "########################################### update #{params.inspect}"
-    @order=Order.find(params[:id])
-
-
-    @order_line_items = @order.line_items.build(params[:order][:line_items])
-    @order_line_items.sub_total = @order_line_items.calculate_subtotal
-    @last_line_item = LineItem.last.product_id
-    @line_items = LineItem.all
-
-      @line_items.each do |line_item|
-        if line_item.product_id == LineItem.last.product_id
-          line_item.product_quantity = LineItem.last.product_quantity + line_item.product_quantity
-        else
-          #@order_line_items = @order.line_items.build(params[:order][:line_items])
-          #@order_line_items.sub_total = @order_line_items.calculate_subtotal
-        end
-
+    @new_item_quantity = (params[:order][:line_items][:product_quantity])
+    logger.info "########################################### new_quantity #{@new_item_quantity.inspect}"
+    @new_item_product_id = (params[:order][:line_items][:product_id])
+    logger.info "########################################### new_item_id #{ @new_item_product_id.inspect}"
+    @order=Order.find(params[:id]) rescue nil
+    @line_items_all=@order.line_items
+    logger.info "########################################### items #{ @line_items_all.inspect}"
+    @line_items_all.each do |line_item|
+      if line_item.product_id ==  @new_item_product_id
+         line_item.product_quantity =  @new_item_quantity +  line_item.product_quantity
+         logger.info "########################################### add #{ line_item.product_quantity.inspect}"
+      else
+        @order_line_items = @order.line_items.build(params[:order][:line_items])
+        @order_line_items.sub_total = @order_line_items.calculate_subtotal
       end
-
-
-    logger.info "########################################### update #{@last_line_item.inspect}"
-    @order_line_items.save
-    flash[:success] = "Added to cart!"
-
-
+    end
+    ##@order_line_items = @order.line_items.build(params[:order][:line_items])
+    #@order_line_items.sub_total = @order_line_items.calculate_subtotal
+    #@order_line_items.save
     respond_to do |format|
       format.js
     end
   end
-
-
-
 end
